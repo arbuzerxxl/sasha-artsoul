1fe9696763204826f59f439945798ebc9e6e640f
s1r1us <k-s-s95@mail.ru>
2022-11-20 17:15:06 +0300

    FIX: DEBUG changed for telebot

--- telebot/settings.py
+++ telebot/settings.py
@@ -3,7 +3,7 @@ from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent
 
-DOCKER = False  # Turn on if runs in Docker for change url
+DOCKER = int(os.environ.get("DEBUG", default=0))
 BOT_TOKEN = os.environ.get('BOT_TOKEN')
 USERNAME = os.environ.get('DJ_ADMIN_USERNAME')
 PASSWORD = os.environ.get('DJ_ADMIN_PASSWORD')
